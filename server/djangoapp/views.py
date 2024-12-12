2a112f52d522a273436a5e5671dd7ff780422da3
kelmalon <kel.malon.694@gmail.com>
2024-12-12 16:06:37 -0500

    Fixed linting errors within djangoapp/views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,24 +1,16 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
 
 from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
-
-
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -42,17 +34,19 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logger.info("Logout request received.")
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -61,20 +55,27 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
-    
+    # email_exist = False
+
     try:
-        #Check if user exists
+        # Check if user exists
         User.objects.get(username=username)
         username_exist = True
-    except:
-        #If not, simply log that this is a new user
+    except Exception as err:
+        # If not, simply log that this is a new user
+        print(f"Unexpected {err=}, {type(err)=}")
         logger.debug("{} is new user".format(username))
 
     if not username_exist:
-        #Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        #Login the user & redirect to list page
+        # Create user in auth_user table
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
+        )
+        # Login the user & redirect to list page
         login(request, user)
         data = {
             "ok": True,
@@ -84,21 +85,23 @@ def registration(request):
     else:
         data = {
             "ok": False,
-            "userName": username, 
+            "userName": username,
             "error": "Already Registered"
         }
         return JsonResponse(data)
 
+
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    if(count == 0):
+    if (count == 0):
         initiate()
 
     models = CarModel.objects.select_related('make')
     cars = []
     for model in models:
         cars.append({"CarModel": model.name, "CarMake": model.make.name})
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
@@ -110,44 +113,47 @@ def get_dealerships(request, state="All"):
 
     dealerships = get_request(endpoint)
 
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
-        #console.log(reviews)
+        # console.log(reviews)
         print(reviews)
 
         for review in reviews:
             response = analyze_review_sentiments(review['review'])
-            #console.log(response)
-            review["sentiment"] = response["sentiment"]            
-        return JsonResponse({"status":200,"reviews":reviews})
-    else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+            # console.log(response)
+            review["sentiment"] = response["sentiment"]
 
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        return JsonResponse({"status": 200, "dealer": dealership})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
 def add_review(request):
     print(request.user.is_anonymous)
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 200, "response": response})
+        except Exception as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
